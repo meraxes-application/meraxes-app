a5576ab947f20302c49c8156eb0220322ccadc17
Carlos Henrique <carlos.silva@novaandradina.org>
2023-07-16 19:59:12 -0400

    fix: line

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import App from "./App.vue";
 AFRAME.registerComponent("color-change", {
   init: function () {
     this.el.addEventListener("click", function () {
-      //set material color to random hex value
+      // set material color to random hex value
       var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
       this.setAttribute("material", { color: randomColor });
     });
